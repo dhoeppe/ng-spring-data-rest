68d67a3adbd9bca494c6b7aff0604d5e430b2c3e
Daniel Höppe <mail@danielhoeppe.com>
2020-01-31 02:45:35 +0100

    Add functionality for version 1.

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # ng-spring-data-rest
-This scripts uses endpoints provided by Spring Data REST to create classes and services to be used with Angular. Uses ngx-hal-client.
+ng-spring-data-rest connects to the specified host providing repository endpoints
+by Spring Data REST and converts them to TypeScript classes and services to be used
+in Angular projects.
+
+The generated services utilize @lagoshny/ngx-hal-client.
+
+## Usage
+```
+usage: ng-spring-data-rest [-h] [-v] -b BASEURL [-u USERNAME] [-p PASSWORD]
+                           [-a AUTH_METHOD] [--auth-endpoint AUTH_ENDPOINT]
+                           [--oauth-flow OAUTH_FLOW] [--client CLIENT_NAME]
+                           [--client-password CLIENT_PASSWORD]
+                           [--no-additional-properties]
+                           [--output-dir OUTPUT_DIR] [--model-dir MODEL_DIR]
+                           [--service-dir SERVICE_DIR]
+                           
+
+Angular class and service generator for use with Spring Data REST and the 
+ngx-hal-client. Generates classes based on the provided JSON schema and ALPS 
+profiles. Allows simple modifications of the generated files.
+
+Optional arguments:
+  -h, --help            Show this help message and exit.
+  -v, --version         Show program's version number and exit.
+  -b BASEURL, --base-url BASEURL
+                        The base URL to the Spring Data REST server.
+  -u USERNAME, --user USERNAME
+                        The username to be used when authenticating with the 
+                        Spring Data REST server.
+  -p PASSWORD, --password PASSWORD
+                        The password to be used when authenticating with the 
+                        Spring Data REST server.
+  -a AUTH_METHOD, --auth AUTH_METHOD
+                        The authentication method to use. When using COOKIE 
+                        the body to the authentication endpoint contains a 
+                        JSON object with the properties 'username' and 
+                        'password' with the specified credentials. OAuth2 
+                        does not support scopes.
+  --auth-endpoint AUTH_ENDPOINT
+                        The authentication endpoint URL.
+  --oauth-flow OAUTH_FLOW
+                        The OAuth2 flow to use when authenticating.
+  --client CLIENT_NAME  The client name to use for OAuth2 authentication.
+  --client-password CLIENT_PASSWORD
+                        The client password to use for OAuth2 authentication.
+  --no-additional-properties
+                        A switch to add "additionalProperties": false to 
+                        every JSON schema before it is converted.
+  --output-dir OUTPUT_DIR
+                        Path the the output directory. If the directory does 
+                        not exist, it is created. If not specified, a new 
+                        folder "gen" is created in the current working 
+                        directory and used as output.
+  --model-dir MODEL_DIR
+                        Name of the model directory.
+  --service-dir SERVICE_DIR
+                        Name of the service directory.
+```
--- ng-spring-data-rest.js
+++ ng-spring-data-rest.js
@@ -11,11 +11,20 @@
 
 'use strict';
 
+// Declare constants
+const REGEXP_TYPESCRIPT_INTERFACE_NAME = /^(export interface )(\w+)( {)$/m;
+const PATH_CLASS_TEMPLATE = './templates/class';
+const PATH_SERVICE_TEMPLATE = './templates/service';
+
 // Load dependencies
 const axios = require('axios').default;
 const axiosCookieJarSupport = require('axios-cookiejar-support').default;
 const tough = require('tough-cookie');
 const qs = require('qs');
+const jsonTs = require('json-schema-to-typescript');
+const fs = require('fs');
+const mustache = require('mustache');
+const _ = require('lodash');
 
 // Declare global variables
 let axiosInstance = undefined;
@@ -26,14 +35,18 @@ let axiosInstance = undefined;
  * @param options The command line parameters and further configuration.
  */
 function ngSpringDataRest(options) {
+    // Axios instance setup
     axiosInstance = axios.create({
-        baseURL: options.baseURL,
-        withCredentials: true,
-        timeout: 10000
-    });
+                                     baseURL: options.baseURL,
+                                     withCredentials: true,
+                                     timeout: 10000
+                                 });
     axiosCookieJarSupport(axiosInstance);
     axiosInstance.defaults.jar = new tough.CookieJar();
-
+    
+    // Mustache setup
+    mustache.tags = ['$$@', '@$$'];
+    
     doGenerate(options);
 }
 
@@ -43,37 +56,63 @@ function ngSpringDataRest(options) {
  * @param options The command line parameters and further configuration.
  */
 async function doGenerate(options) {
-    let response;
     if (options.authMethod !== 'NONE') {
         try {
-            response = await doLogin(options.authMethod, options.authEndpoint, options.username, options.password);
+            await doLogin(options);
+            console.log(`Authenticated as user ${options.username}.`);
         } catch {
-            console.error(`Authentication failed.\n\n${response}`);
+            console.error(`Authentication failed.`);
+            process.exit(5);
         }
     }
-
+    
     // Collect models to generate files for.
-    const entities = collectEntities();
-
+    const entities = await collectEntities();
+    console.log('Collected list of entities.');
+    
     // Collect JSON schemas.
-
-    // Generate services.
+    const jsonSchemas = await collectSchemas(entities);
+    
+    // Create output directory.
+    fs.mkdirSync(`${options.outputDir}/${options.modelDir}`,
+                 {recursive: true});
+    fs.mkdirSync(`${options.outputDir}/${options.serviceDir}`,
+                 {recursive: true});
+    
+    // Process JSON schemas based on configuration.
+    preProcessSchemas(jsonSchemas, options);
+    
+    // Convert each schema to TypeScript classes and services.
+    generateTypeScriptFromSchema(jsonSchemas,
+                                 entities,
+                                 options.outputDir,
+                                 options.modelDir,
+                                 options.serviceDir);
 }
 
 /**
  * Performs the login based on the provided authentication method.
  *
- * @param authMethod
- * @param authEndpoint
- * @param username
- * @param password
- * @returns Promise promise for the request.
+ * @param options The command line options.
+ * @returns {Promise} promise for the request.
  */
-function doLogin(authMethod, authEndpoint, username, password) {
+function doLogin(options) {
     // Login if necessary with the specified method.
-    switch (authMethod) {
+    switch (options.authMethod) {
         case 'COOKIE':
-            return authenticateWithCookies(authEndpoint, username, password);
+            return authenticateWithCookies(options.authEndpoint,
+                                           options.username,
+                                           options.password);
+        case 'OAUTH2':
+            return authenticateWithOAuth2(options.oauthFlow,
+                                          options.authEndpoint,
+                                          options.username,
+                                          options.password,
+                                          options.clientId,
+                                          options.clientPassword)
+                .then(response => {
+                    axiosInstance.defaults.headers.common['Authorization'] = response.data.access_token;
+                })
     }
 }
 
@@ -87,22 +126,195 @@ function doLogin(authMethod, authEndpoint, username, password) {
  *     password: "..."
  * }
  *
- * @param authEndpoint The authentication endpoint to use, fully qualified.
+ * @param authEndpoint The authentication endpoint URL to use, fully qualified.
  * @param username
  * @param password
+ * @returns {Promise} Promise for the POST request to the authentication endpoint.
  */
 function authenticateWithCookies(authEndpoint, username, password) {
     return axiosInstance.post(authEndpoint,
-        qs.stringify({username: username, password: password}));
+                              qs.stringify({
+                                               username: username,
+                                               password: password
+                                           }));
 }
 
-function collectEntities() {
-    axiosInstance.get('profile')
-        .then(response => {
-            // TODO process
-        });
+/**
+ * OAuth2 authentication
+ *
+ * Currently only supports the PASSWORD flow without scopes.
+ *
+ * @param flow The authorization flow to use when authenticating.
+ * @param authEndpoint The authentication endpoint URL to use, fully qualified.
+ * @param username
+ * @param password
+ * @param client
+ * @param clientPassword
+ * @returns {Promise} Promise for the POST request to the authentication endpoint.
+ */
+function authenticateWithOAuth2(flow, authEndpoint, username, password, client, clientPassword) {
+    switch (flow) {
+        case 'PASSWORD':
+            return axiosInstance.post(authEndpoint,
+                                      qs.stringify({
+                                                       grant_type: 'password',
+                                                       username: username,
+                                                       password: password,
+                                                       client_id: client,
+                                                       client_secret: clientPassword
+                                                   }),
+                                      {
+                                          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                                          auth: {
+                                              username: client,
+                                              password: clientPassword
+                                          }
+                                      });
+    }
+}
 
-    return [];
+/**
+ * Retrieves an array of repository endpoint names provided by Spring Data REST using
+ * the <host>/<basePath>/profile endpoints.
+ *
+ * @returns {Promise<[]>} Promise for an array of strings containing the repository names.
+ */
+function collectEntities() {
+    return axiosInstance.get('profile')
+        .then(response => {
+            if (!('_links' in response.data)) {
+                console.error(
+                    'Response does not contain _links element. Could not collect entities.');
+                process.exit(4);
+            }
+            
+            const keys = Object.keys(response.data._links);
+            removeElementFromArray(keys, 'self');
+            return keys;
+        })
+        .catch(() => {
+            console.error('Collecting entities failed.');
+            process.exit(3);
+        });
+}
+
+/**
+ * Retrieves the JSON schema provided by Spring Data REST for each of the entities in the given array.
+ *
+ * @param entities An array containing strings with the name of each repository in Spring Data REST.
+ * @returns {Promise<[]>} Promise for an array of JSON schemas.
+ */
+async function collectSchemas(entities) {
+    const schemas = [];
+    console.log('Collecting schemas.');
+    
+    for (const element of entities) {
+        await axiosInstance.get(`profile/${element}`,
+                                {headers: {'Accept': 'application/schema+json'}})
+            .then(response => {
+                schemas.push(response.data);
+            })
+            .catch(() => {
+                console.error(`Could not collect schema for '${element}'.`);
+                process.exit(4);
+            });
+    }
+    
+    return schemas;
+}
+
+/**
+ * Pre-Processes schemas according to the given configuration.
+ *
+ * @param schemas
+ * @param config
+ */
+function preProcessSchemas(schemas, config) {
+    for (const schema of schemas) {
+        if (config.noAdditionalProperties) {
+            schema.additionalProperties = false;
+        }
+    }
+}
+
+/**
+ * Generates TypeScript classes in the 'model' directory from the given JSON schemas.
+ *
+ * @param schemas The JSON schemas to convert.
+ * @param entities The array of entities, must match the schemas array.
+ * @param outputDir The output directory to use. Models are generated in the 'model' subdirectory.
+ * @param modelDir The name of the model directory.
+ * @param serviceDir The name of the service directory.
+ */
+function generateTypeScriptFromSchema(schemas, entities, outputDir, modelDir, serviceDir) {
+    console.log(`Generating files.`);
+    
+    const classTemplateString = fs.readFileSync(PATH_CLASS_TEMPLATE).toString();
+    const serviceTemplateString = fs.readFileSync(PATH_SERVICE_TEMPLATE).toString();
+    
+    for (let index = 0; index < schemas.length; index++) {
+        const schema = schemas[index];
+        const entity = entities[index];
+        
+        // Apply json-schema-to-typescript conversion.
+        jsonTs.compile(schema, schema.title)
+            .then(tsFile => {
+                // Add I to the beginning of each class name to indicate interface.
+                tsFile = tsFile.replace(REGEXP_TYPESCRIPT_INTERFACE_NAME,
+                                        '$1I$2$3');
+                
+                // Construct filename for generated interface file.
+                let matches = tsFile.match(REGEXP_TYPESCRIPT_INTERFACE_NAME);
+                
+                const interfaceName = matches[2];
+                const interfaceNameLower = interfaceName.toLowerCase();
+                const className = interfaceName.substr(1);
+                const classNameLower = className.toLowerCase();
+                const classNameSnake = _.camelCase(className);
+                
+                // Write interface file.
+                const interfaceFileName = `${interfaceNameLower}.d.ts`;
+                fs.writeFileSync(`${outputDir}/${modelDir}/${interfaceFileName}`,
+                                 tsFile);
+                
+                // Create class from template file.
+                const classTemplateData = {
+                    'interfaceName': interfaceName,
+                    'interfaceNameLower': interfaceNameLower,
+                    'className': className
+                };
+                const renderedClass = mustache.render(classTemplateString,
+                                                      classTemplateData);
+                const classFileName = `${classNameSnake}.ts`;
+                fs.writeFileSync(`${outputDir}/${modelDir}/${classFileName}`,
+                                 renderedClass);
+                
+                // Create service from template file.
+                const serviceTemplateData = {
+                    'className': className,
+                    'classNameLower': classNameLower,
+                    'repositoryName': entity
+                };
+                const renderedService = mustache.render(serviceTemplateString,
+                                                        serviceTemplateData);
+                const serviceFileName = `${classNameSnake}.service.ts`;
+                fs.writeFileSync(`${outputDir}/${serviceDir}/${serviceFileName}`,
+                                 renderedService);
+            });
+    }
+}
+
+/**
+ * Removes an element from an array in-place.
+ *
+ * @param array The array to process.
+ * @param element The element to remove from the array.
+ */
+function removeElementFromArray(array, element) {
+    const elementIndex = array.indexOf(element);
+    if (elementIndex > -1) {
+        array.splice(elementIndex, 1);
+    }
 }
 
 module.exports = ngSpringDataRest;
